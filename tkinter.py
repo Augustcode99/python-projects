bc5281ac28e955bb3cab90d7f6d9943091adc541
Augustcode99 <107293666+Augustcode99@users.noreply.github.com>
2022-07-06 21:26:44 +0300

    deleted unfinished project

--- tkinter.py
+++
@@ -1,9 +0,0 @@
-# import tkinter
-# window = tkinter.Tk()
-# window.geometry("500x400")
-# window.title("Spam Bot")
-# label = tkinter.Label(window, text = "lets code a GUI !!").pack()
-# #labels are used to create text and images
-# but = (window,text="Dont Click Or Else")
-# but.grid(column=1, row=0)
-# window.mainloop()
